690658486bc1a8e2c676dd1bcd6bca78805d64b5
Vanderson Gonçalves <vandersondev@gmail.com>
2025-12-14 22:50:48 -0300

    immutable test

---
+++ immutable.exs
@@ -0,0 +1,32 @@
+Mix.install([
+  {:liveview_playground, "~> 0.1.8"}
+])
+
+defmodule PageLive do
+  use LiveviewPlaygroundWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    assign(socket, name: "Immutable")
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Document</title>
+        </head>
+        <body>
+          <h1>Hello <%= @name %>!</h1>
+          <p>You are <%= @age %></p>
+        </body>
+      </html>
+    """
+  end
+
+end
+
+LiveviewPlayground.start()
